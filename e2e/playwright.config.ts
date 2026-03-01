0b05463b16bb3dc83b9944e942dbdfd37cbadecd
a13o <2031379+a13o@users.noreply.github.com>
2026-03-01 23:18:45 +0100

    don't leave server running on failures

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', {open: 'never'}]],
   timeout: 30_000,
   expect: {
     timeout: 10_000,
